89291f3f03a9721e9a1325a6c0d26a9f470b2149
julianencisoh <encisojulian1@gmail.com>
2021-11-21 21:33:21 -0500

    single product view ready

---
+++ js/catalogo.js
@@ -0,0 +1,43 @@
+const productos = [
+    {
+        id: 1,
+        name: "Jarron Oro Rosa",
+        price: 12000,
+        size: "Mediano",
+        image: "./recursos/JarronOroRosa.png",
+        ref: "0301",
+        description: "Nuestro Jarrón oro rosa es el complemento ideal para tu decoración en tonos tierras, nudes, rosas, o como quieras combinarlo. Puedes llevarlo con las flores artificiales color rosa o decorarlo con flores naturales a tu elección. Te recomendamos el cuidado de este producto y transportarlo en su empaque.",
+        type: "Jarrones"
+    },
+    {
+        id: 2,
+        name: "Candelabro 3 Niveles",
+        price: 10000,
+        size: "Mediano",
+        image: "./recursos/CandelabroTresNiveles.png",
+        ref: "0305",
+        description: "Nuestro Jarrón oro rosa es el complemento ideal para tu decoración en tonos tierras, nudes, rosas, o como quieras combinarlo. Puedes llevarlo con las flores artificiales color rosa o decorarlo con flores naturales a tu elección. Te recomendamos el cuidado de este producto y transportarlo en su empaque.",
+        type: "Candelabros"
+    },
+    {
+        id: 3,
+        name: "Jarron Espejos",
+        price: 15000,
+        size: "Mediano",
+        image: "./recursos/JarronEspejos.png",
+        ref: "0302",
+        description: "Nuestro Jarrón oro rosa es el complemento ideal para tu decoración en tonos tierras, nudes, rosas, o como quieras combinarlo. Puedes llevarlo con las flores artificiales color rosa o decorarlo con flores naturales a tu elección. Te recomendamos el cuidado de este producto y transportarlo en su empaque.",
+        type: "Jarrones"
+    },
+    {
+        id: 4,
+        name: "Mesa Vintage",
+        price: 30000,
+        size: "Grande",
+        image: "./recursos/Vintage.png",
+        ref: "0309",
+        description: "Nuestro Jarrón oro rosa es el complemento ideal para tu decoración en tonos tierras, nudes, rosas, o como quieras combinarlo. Puedes llevarlo con las flores artificiales color rosa o decorarlo con flores naturales a tu elección. Te recomendamos el cuidado de este producto y transportarlo en su empaque.",
+        type: "Mesas"
+    }
+    
+];
---
+++ js/producto.js
@@ -0,0 +1,17 @@
+const url = window.location.search;
+var searchParams = new URLSearchParams(url);
+
+const productoId = searchParams.get("id");
+
+const producto = productos.find(producto => producto.id == productoId); 
+
+const imagenProducto = document.getElementById("imagenProducto");
+const nombreProducto = document.getElementById("nombreProducto");
+const descripcionProducto = document.getElementById("descripcionProducto");
+const precioProducto = document.getElementById("precioProducto");
+
+nombreProducto.innerText = producto.name;
+descripcionProducto.innerText = producto.description;
+precioProducto.innerText = `$ ${producto.price}`;
+
+imagenProducto.setAttribute("src", producto.image);
