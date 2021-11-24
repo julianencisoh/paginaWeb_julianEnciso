aa64963133f267392efa074d171798a9b7a97c7a
julianencisoh <encisojulian1@gmail.com>
2021-11-24 00:53:43 -0500

    all products charged from firebase

--- js/producto.js
+++ js/producto.js
@@ -1,17 +1,44 @@
-const url = window.location.search;
-var searchParams = new URLSearchParams(url);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
+import { getFirestore, doc, getDoc} from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
 
-const productoId = searchParams.get("id");
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-const producto = productos.find(producto => producto.id == productoId); 
+const getProduct = async () => {
+    const url = window.location.search;
+    var searchParams = new URLSearchParams(url);
+    const productoId = searchParams.get("id");
+
+    const docRef = doc(db, "productos", productoId);
+    const docSnap = await getDoc(docRef);
+    const data = docSnap.data();
+
+    loadProductInfo(data);
+
+    console.log(data);
+}
+
+
+
+
+//const producto = productos.find(producto => producto.id == productoId); 
 
 const imagenProducto = document.getElementById("imagenProducto");
 const nombreProducto = document.getElementById("nombreProducto");
 const descripcionProducto = document.getElementById("descripcionProducto");
 const precioProducto = document.getElementById("precioProducto");
 
-nombreProducto.innerText = producto.name;
-descripcionProducto.innerText = producto.description;
-precioProducto.innerText =`${ formatCurrency(producto.price) }`;
-imagenProducto.setAttribute("src", producto.image);
 
+const loadProductInfo = (producto) => {
+
+    nombreProducto.innerText = producto.name;
+    descripcionProducto.innerText = producto.description;
+    precioProducto.innerText =`${ formatCurrency(producto.price) }`;
+    imagenProducto.setAttribute("src", producto.image);
+};
+
+
+
+
+
+getProduct();
